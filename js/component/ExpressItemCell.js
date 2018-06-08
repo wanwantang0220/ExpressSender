c5b4fcfd0519363781631ae9050d6975fa66fa30
tww <710752055@qq.com>
2018-06-08 17:46:32 +0800

    update 发件页面

---
+++ js/component/ExpressItemCell.js
@@ -0,0 +1,47 @@
+
+
+import Reacr,{PureComponent} from 'react';
+import {White} from "../style/BaseStyle";
+import {Image, Text, View} from "react-native";
+
+
+const PIC=require('../../img/');
+export default  class ExpressItemCell extends PureComponent{
+
+    render(){
+        return(
+            <View style={{marginTop:10}}>
+
+                <View style={{backgroundColor:White ,flexDirection:'row'}}>
+                    <View>
+                        <Text>订单号:</Text>
+                        <Text>D1111111111</Text>
+                    </View>
+                    <View style={{flexDirection:'row'}}>
+                        <View style={{flex:1,flexDirection:'column'}}>
+                            <Text>无锡</Text>
+                            <Text>测试</Text>
+                        </View>
+                        <View style={{flex:2}}>
+                            <Text>待揽收</Text>
+                            <Image
+                                style={{position:'absolute',
+                                    top: 200,
+                                    left: 40,
+                                    backgroundColor: 'green'}}
+                                source={PIC} resizeMode={Image.resizeMode.contain}/>
+                        </View>
+                        <View style={{flex:1,flexDirection:'column'}}>
+                            <Text>北京</Text>
+                            <Text>测试</Text>
+                        </View>
+                    </View>
+                    <View>
+                        <Text>下单时间:</Text>
+                        <Text>2018-06-01 16:58:58</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
